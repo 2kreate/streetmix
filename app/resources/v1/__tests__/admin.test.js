b0862c1596908c1d5cdc3eb2b84298aa758d0bf8
Daniel Powell <daniel@2kreate.com>
2018-04-16 03:42:10 -0600

    Test working

--- app/resources/v1/__tests__/admin.test.js
+++ app/resources/v1/__tests__/admin.test.js
@@ -2,23 +2,61 @@
 import request from 'supertest'
 import express from 'express'
 import admin from '../admin'
+import bodyParser from 'body-parser'
 
 function setupMockServer () {
   const app = express()
 
-  app.post('api/v1/admin', admin.post)
+  app.use(bodyParser.json())
+  app.post('/api/v1/admin', admin.post)
+  app.get('/api/v1/admin', function (req, res) { res.status(400).send('You do not belong here!') })
 
   return app
 }
 
-describe('post api/v1/admin', function () {
+describe('test /api/v1/admin GET and POST', function () {
   const app = setupMockServer()
 
+  // I understand that this GET test is somewhat arbitrary,
+  // but I left it anyway
   it('makes a GET request', () => {
     return request(app)
-      .post('/api/v1/admin')
+      .get('/api/v1/admin')
       .then((response) => {
         expect(response.statusCode).toEqual(400)
+      })
+  })
+
+  it('respond with 201 created', function (done) {
+    request(app)
+      .post('/api/v1/admin')
+      .send({
+        email: 'test@email.com',
+        name: 'name',
+        dob: '2018-04-16T08:44:55+00:00'
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .end((err) => {
+        if (err) return done(err)
+        done()
+      })
+  })
+
+  it('respond with 201 created', function (done) {
+    request(app)
+      .post('/api/v1/admin')
+      .send({
+        name: 'name',
+        dob: '2018-04-16T08:44:55+00:00'
+      })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .end((err) => {
+        if (err) return done(err)
+        done()
       })
   })
 })
--- app/resources/v1/admin.js
+++ app/resources/v1/admin.js
@@ -2,41 +2,32 @@ var isEmail = require('validator/lib/isEmail')
 var isDate = require('validator/lib/isISO8601')
 
 exports.post = function (req, res) {
-  // var email
-  // var name
-  // var dob
-  var success = true
-
+  var status = 400
+  var statusMsg = ''
   if (req.body.email) {
-    if (isEmail(req.body.email)) {
-      // email = req.body.email
-    } else {
-      res.status(400).json({ msg: 'Not a valid email address: ' + req.body.email })
+    if (!isEmail(req.body.email)) {
+      statusMsg += 'Not a valid email address: ' + req.body.email + '. '
     }
   } else {
-    res.status(400).json({ msg: 'No email address provided.' })
-    success = false
+    statusMsg += 'No email address provided. '
   }
 
-  if (req.body.name) {
-    // name = req.body.name
-  } else {
-    res.status(400).json({ msg: 'No name provided.' })
-    success = false
+  if (!req.body.name) {
+    statusMsg += 'No name provided. '
   }
 
   if (req.body.dob) {
-    if (isDate(req.body.dob)) {
-      // dob = req.body.dob
-    } else {
-      res.status(400).json({ msg: 'Not a valid date: ' + req.body.dob })
+    if (!isDate(req.body.dob)) {
+      statusMsg += 'Not a valid date: ' + req.body.dob + '. '
     }
   } else {
-    res.status(400).json({ msg: 'No date of birth provided.' })
-    success = false
+    statusMsg += 'No date of birth provided.'
   }
 
-  if (success) {
-    res.status(201).json({ msg: 'Thank you ' + req.body.name + '. Your information was recieved!' })
+  if (statusMsg === '') {
+    status = 201
+    statusMsg += 'Thank you ' + req.body.name + '. Your information was recieved!'
   }
+
+  res.status(status).json({ msg: statusMsg })
 }
